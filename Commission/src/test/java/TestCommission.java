7c944e2707b74b66808a46d62b3a134cdd23e7a3
Scott Fennell <105092924+scottfennell5@users.noreply.github.com>
2023-11-16 16:08:32 -0500

    init
    innit

---
+++ Commission/src/test/java/TestCommission.java
@@ -0,0 +1,50 @@
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestCommission {
+
+    @Mock
+    private Lock lock = Mockito.mock(Lock.class);
+
+    @Mock
+    private Stock stock = Mockito.mock(Stock.class);
+
+    @Mock
+    private Barrel barrel = Mockito.mock(Barrel.class);
+
+    @InjectMocks
+    CalculateCommission c = new CalculateCommission();
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetCommissionSkipWhileLoop() {
+
+    }
+
+    @Test
+    public void testGetCommissionEnterWhileLoop() {
+        when(lock.getCount()).thenReturn(5);
+        Lock l = spy(new Lock(5));
+        when(stock.getCount()).thenReturn(5);
+        when(barrel.getCount()).thenReturn(5);
+        double com = c.getCommission(lock, stock, barrel);
+        Assert.assertEquals(520, com, 0);
+    }
+}
